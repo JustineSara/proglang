11d35c139dfd2a0029754f03770525b7a2baf637
Justine S. Pujos <pujos.justine.s@gmail.com>
2025-06-05 14:10:16 +0200

    step1: solve

--- src/main.clj
+++ src/main.clj
@@ -1,6 +1,13 @@
 (ns main
   (:require [instaparse.core :as insta]))
 
+(def ope
+  (insta/parser
+    "S = A ('+' A)*
+    A = D | (D ('*' D)*)
+    D = #'\\d+'
+    "))
+
 (defn run
   [opts]
   (print "Hello World!"))
--- test/main_test.clj
+++ test/main_test.clj
@@ -1,5 +1,10 @@
 (ns main-test
-  (:require [clojure.test :refer [deftest is]]))
+  (:require [clojure.test :refer [deftest is]]
+            [main :as m]))
 
 (deftest istrue
   (is (= 1 1)))
+
+(deftest step1
+  (is (= [:S [:A [:D "1"]] "+" [:A [:D "1"]]]
+        (m/ope "1+1"))))
