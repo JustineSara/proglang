646324500599066a874861e9e72ff156208ad73d
JustineSara <122374232+JustineSara@users.noreply.github.com>
2025-06-05 15:19:30 +0200

    Step1 (#7)
    * step1: goal
    
    * step1: solve
    
    * step1: more tests
    
    * step1: better grammar
    
    * step1: result

--- src/main.clj
+++ src/main.clj
@@ -1,6 +1,15 @@
 (ns main
   (:require [instaparse.core :as insta]))
 
+(def ope
+  (insta/parser
+    "S = A|M|D
+    A = (D|M) <W*> (<'+'> <W*> (D|M))+
+    M = D <W*> (<'*'> <W*> D)+
+    D = #'\\d+'
+    W = #' '
+    "))
+
 (defn run
   [opts]
   (print "Hello World!"))
--- test/main_test.clj
+++ test/main_test.clj
@@ -1,5 +1,18 @@
 (ns main-test
-  (:require [clojure.test :refer [deftest is]]))
+  (:require [clojure.test :refer [deftest is]]
+            [main :as m]))
 
 (deftest istrue
   (is (= 1 1)))
+
+(deftest step1
+  (is (= [:S [:D "3"]]
+         (m/ope "3")))
+  (is (= [:S [:A [:D "1"] [:D "1"]]]
+         (m/ope "1+1")))
+  (is (= [:S [:A [:D "1"] [:D "2"]]]
+        (m/ope "1 + 2")))
+  (is (= [:S [:M [:D "2"] [:D "2"] [:D "2"]]]
+         (m/ope "2 * 2*2")))
+  (is (= [:S [:A [:M [:D "2"] [:D "1"]] [:D "1"]]]
+         (m/ope "2 * 1 + 1"))))
