2ef1f843b4709ba5d0566625f049d9036968098d
Justine S. Pujos <pujos.justine.s@gmail.com>
2025-06-12 11:33:30 +0200

    step4: solution part2 better

--- src/main.clj
+++ src/main.clj
@@ -4,7 +4,7 @@
 
 (def ope
   (insta/parser
-    "S = A|M|D|Dp|Ap|Mp
+    "S = A|M|D|Dp|Ap|Mp|Q
     <Ap> = <'('> A <')'>
     A = (D|Dp|M|Mp|Ap) <W*> (<'+'> <W*> (D|Dp|M|Mp|Ap))+
     M = (D|Dp|Mp|Ap) <W*> (<'*'> <W*> (D|Dp|Ap|Mp))+
@@ -12,6 +12,7 @@
     <Dp> = <'('> D <')'>
     D = #'\\d+'
     W = #' '
+    Q = 'quit!'|'q!'
     "))
 
 (defn opeeval
@@ -20,7 +21,8 @@
     (insta/transform {:D (fn [x] (edn/read-string x))
                       :A (fn [x & r] (apply + x r))
                       :M (fn [x & r] (apply * x r))
-                      :S (fn [x] x)}
+                      :S (fn [x] x)
+                      :Q (fn [x] :stop)}
                      tree)))
 
 (defn run
@@ -29,13 +31,13 @@
          \n\nFor now, only the + and * operations exist.\nHave fun!\n\n")
   (loop [stop false]
     (if stop
-      (do (print "\nBye!!\n")
-          :stop)
+      :stop
       (let [_ (print "> ")
             _ (flush)
-            inp (read-line)]
-        (if (= inp "q!")
-          (recur true)
-          (let [outp (opeeval inp)
-                _ (print " " outp "\n")]
-            (recur stop)))))))
+            inp (read-line)
+            outp (opeeval inp)
+            mss (if (= outp :stop)
+                  "\nBye!!\n"
+                  (str " " outp "\n"))]
+        (print mss)
+        (recur (= outp :stop))))))
